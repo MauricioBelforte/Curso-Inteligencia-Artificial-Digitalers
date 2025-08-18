15b48882e419d4ed57834b33e7c0be38724520e2
Mauricio Belforte <belymau@hotmail.com>
2025-08-18 14:21:51 -0300

    Practica 1 - Introduccion a Python

---
+++ Introduccion a Python/Practica 1- Introduccion a Python/1-clase1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+# Programa para ver un hola mundo en python
+print("Hola, mundo!")
+
+variable = "Hola, maury!"
+
+print(variable)
+
+
+# Función para sumar dos números
+
+# Definición de una función en Python
+# En Python, se define una función utilizando la palabra clave 'def', seguida del nombre de la función, paréntesis y dos puntos.
+# Aquí hay un ejemplo de una función que suma dos números:
+def suma(a, b):
+    """Suma dos números y devuelve el resultado."""
+    return a + b        
+# Llamada a la función suma
+resultado = suma(3, 5)
+
+# Imprimir el resultado de la suma
+print("El resultado de la suma es:", resultado)
